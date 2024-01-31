e4e500382584925eea0d6bca285af83ce59be1b6
Vyacheslav <111475808+Johnynight@users.noreply.github.com>
2024-01-31 22:10:43 +0600

    k

--- reseption/mp/views.py
+++ reseption/mp/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
-from .forms import Form_mp
-from django.views.generic import DetailView, ListView, TemplateView, CreateView
+from .forms import MyForm
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from docxtpl import DocxTemplate
+import io
 
 
-class Main(CreateView):
-    form_class = Form_mp
-    template_name = 'mp/index.html'
+def main(request):
+    if request.method == "POST":
+        form = MyForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            content = form.cleaned_data['content']
+
+            document = DocxTemplate('reseption/mp/word_templates/template_docx.docx')
+            context = {
+                'number' : name
+            }
+            # document.add_heading(f'Отчет от {name}', level=1)
+            # document.add_paragraph(f'Имя: {name}')
+            # document.add_paragraph(f'Email: {email}')
+            # document.add_paragraph(f'Текст: {content}')
+            document.render(context)
+
+            # Создаем байтовый поток
+            output = io.BytesIO()
+            document.save(output)
+            output.seek(0)
+
+            # Отправляем файл в загрузку
+            response = HttpResponse(
+                content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+            response['Content-Disposition'] = f'attachment; filename={name}.docx'
+            response.write(output.read())
+
+            return response
+    else:
+        form = MyForm()
+
+    return render(request, 'mp/index.html', {'form': form})
