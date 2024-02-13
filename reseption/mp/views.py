448eb9c2ee0a5311aa38bb52afd54ee59e6aad84
vchurikov <teqilkka@gmail.com>
2024-02-13 10:00:20 +0600

    as

--- reseption/mp/views.py
+++ reseption/mp/views.py
@@ -30,7 +30,7 @@ def main(request):
             soglasoval = form.cleaned_data['soglasoval']
             # / home / teqilkka / media / template_docx.docx
             document = DocxTemplate(
-                'reseption/mp/word_templates/template_docx.docx')  # /home/teqilkka/media/template_docx.docx
+                '/home/teqilkka/media/template_docx.docx')  # /home/teqilkka/media/template_docx.docx
             if form.cleaned_data['osnovamie'] == 'custom':
                 osnovamie = form.cleaned_data['osnovamie_dop']
             if vid_transfer == 'custom':
