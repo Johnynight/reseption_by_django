f86dd6ff53967a4b88284eab456e70aa2a4c2c25
Vyacheslav <111475808+Johnynight@users.noreply.github.com>
2024-02-24 13:52:58 +0600

    k

--- reseption/mp/admin.py
+++ reseption/mp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import Material_pass
+from django.http import HttpResponse
+import csv
 
 
 class Mp_admin(admin.ModelAdmin):
@@ -32,5 +31,34 @@ class Mp_admin(admin.ModelAdmin):
 
     list_display_links = ('name_property',)  # Установка поля name_property в качестве ссылки на редактирование
 
-admin.site.register(Material_pass, Mp_admin)
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="data.csv"'
 
+        fieldnames = [  # здесь вы можете указать имена полей вашей модели Material_pass
+            'serial_number',
+            'name_property',
+            'reason',
+            'owner_reason',
+            'time_registration',
+            'type_transport',
+            'where',
+            'issued_pass',
+            'today_data',
+            'approved'
+        ]
+
+        writer = csv.DictWriter(response, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for obj in queryset:
+            writer.writerow({field: getattr(obj, field) for field in fieldnames})
+
+        return response
+
+    export_to_csv.short_description = "Экспортировать в CSV"  Определяем отображаемое имя для действия
+
+    actions = ['export_to_csv']
+
+
+admin.site.register(Material_pass, Mp_admin)
